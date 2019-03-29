58d04abb8f52797664d90260bd495279e4efd257
jiangtj <116749895@qq.com>
2019-03-29 13:09:42 +0800

    Git

---
+++ README.md
@@ -0,0 +1,3 @@
+# Git 模式
+
+使用命令`mvn clean && mvn package -P git`，下载远程contract，并上传
--- src/test/java/com/jtj/cloud/springcontractexample/AbstractDnocmTest.java
+++ src/test/java/com/jtj/cloud/springcontractexample/AbstractDnocmTest.java
@@ -4,13 +4,9 @@ import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebFlux;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.context.WebApplicationContext;
@@ -26,7 +22,7 @@ import org.springframework.web.context.WebApplicationContext;
 )
 @AutoConfigureMockMvc
 @AutoConfigureStubRunner
-@ActiveProfiles("stub")
+@ActiveProfiles("git")
 public abstract class AbstractDnocmTest {
 
     @Autowired
